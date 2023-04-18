2154ce4fde86f94f09883557dfe1ae8723e88619
Bhabaranjan Panigrahi <112833588+bhaba-ranjan@users.noreply.github.com>
2023-04-18 16:50:06 -0400

    fix: transformation file is fixed

--- scripts/data-builder/transform_data.py
+++ scripts/data-builder/transform_data.py
@@ -13,10 +13,10 @@ coloredlogs.install()
 def read_images(path):
     image = cv2.imread(path)
     # Will have to do some re-sizing
-    return cv2.cvtColor(path, cv2.COLOR_BGR2RGB)
+    return cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-def get_transformation_matrix(position, orientation):
-    theta = R.from_quat(orientation).as_euler('XYZ')[2]
+def get_transformation_matrix(position, quaternion):
+    theta = R.from_quat([quaternion.x, quaternion.y, quaternion.z, quaternion.w]).as_euler('XYZ')[2]
     robo_coordinate_in_glob_frame  = np.array([[np.cos(theta), -np.sin(theta), position.x],
                     [np.sin(theta), np.cos(theta), position.y],
                     [0, 0, 1]])
@@ -36,7 +36,7 @@ class transform_data(Dataset):
         self.prev_cmd_vel = input_data[3]        
         self.robot_position  = input_data[4]
         self.gt_cmd_vel = input_data[5]
-        self.grid_size = grid_size        
+        self.grid_size = np.asarray(grid_size)        
     
     def __len__(self):
          # TODO: this will return 1 example set with the following details
@@ -46,14 +46,13 @@ class transform_data(Dataset):
         # Transform images
         images = [read_images(path) for path in self.image_paths]
         stacked_images = np.stack((images), axis=2)
-        
+
         # Transform local goal into robot frame
         robot_coordinate_in_glob_frame = get_transformation_matrix(self.robot_position[0],self.robot_position[1])
         transform_to_robot_coordinate =   np.linalg.pinv(robot_coordinate_in_glob_frame)
 
-        local_goal_coordinate_in_glob_frame = get_transformation_matrix(self.local_goal[0],self.local_goal[1])
-        local_goal_in_robot_frame = transform_to_robot_coordinate @ local_goal_coordinate_in_glob_frame        
-        local_goal = (local_goal_in_robot_frame[0,2], local_goal_in_robot_frame[1,2])
+        local_goal_in_robot_frame = transform_to_robot_coordinate @ np.asanyarray([self.local_goal[0], self.local_goal[1],1]).reshape((3,1))	
+        local_goal = (local_goal_in_robot_frame[0,0], local_goal_in_robot_frame[1,0])
 
         # Transform point-clouds to 3D-Cylider co-ordinate system
         point_clouds = np.concatenate(self.point_clouds)
