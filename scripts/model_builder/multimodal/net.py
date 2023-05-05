b73450c20dfa836d9f96cd1254338f84097cf665
Bhabaranjan Panigrahi <112833588+bhaba-ranjan@users.noreply.github.com>
2023-05-05 16:30:55 -0400

    fix: mulitmodality added

---
+++ scripts/model_builder/multimodal/net.py
@@ -0,0 +1,49 @@
+# from typing import List
+
+import torch
+import torch.nn as nn
+from ..image.backbone import make_mlp, get_backbone
+from ..pcl.pointnet import PointNetDenseCls
+
+
+
+class BCModelPcl(nn.Module):
+    def __init__(
+        self,        
+        backbone_img: str = "resnet18",
+        sparse_shape = [480, 360, 32],
+        controller_encoder: list = [(198256+512), 256, 128, 64, 2],
+        goal_encoder: list = [2, 256, 128],
+        prev_cmd_encoder: list = [2 * 18, 64, 128],
+        n_frames: int = 4,
+        n_channels: int = 3,
+        act: str = 'relu',
+        l_act: bool = False, bn: bool = False, dropout: float = 0.0
+    ):
+
+        super().__init__()
+        self.backbone_pcl = PointNetDenseCls().float()
+        self.backbone_img = get_backbone(backbone_img, n_frames, n_channels)
+        self.controller = make_mlp(controller_encoder, act, l_act, bn, dropout)
+        self.goal_encoder = make_mlp(goal_encoder, act, l_act, bn, dropout)
+        self.prev_cmd_encoder = make_mlp(prev_cmd_encoder, act, l_act, bn, dropout)
+
+    def forward(self, pcl, stacked_images,local_goal, prev_cmd_vel):
+        # print(f"{img.stacked_images = }")
+        # image features in shape (B, 512)
+        imgs_feat = self.backbone_img(stacked_images)
+        point_cloud_feat = self.backbone_pcl(pcl.float())
+        # goal features in shape (B, 128)
+        goal = self.goal_encoder(local_goal)
+        # prec_cmd_vel features in shape (B, 128)
+        print(prev_cmd_vel.shape)
+        prev_cmd = self.prev_cmd_encoder(prev_cmd_vel)
+        # concat all encoded features together along the last dim,
+        # making a tensor of shape (B, 512 + 128 + 128) = (B, 768)
+        # print(point_cloud.shape)
+        # print(goal.shape)
+        # print(prev_cmd.shape)
+        multimodal_feat = torch.cat([point_cloud_feat,imgs_feat], dim=1)
+        features = torch.cat([multimodal_feat, goal, prev_cmd], dim=-1)
+        # return the action in shape (B, 2)
+        return self.controller(features)
---
+++ scripts/multimodal.py
@@ -0,0 +1,46 @@
+import torch
+from data_builder.indexer import IndexDataset
+from data_builder.transformer import ApplyTransformation
+from model_builder.multimodal.net import BCModelPcl
+from torch.utils.data import DataLoader
+import matplotlib.pyplot as plt
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+
+root = '/Users/bhabaranjanpanigrahi/Research/Code/fusion-network/recorded-data/136021.bag'
+
+indexer = IndexDataset(root)
+train_dataset = ApplyTransformation(indexer)
+train_loader = DataLoader(train_dataset, batch_size=2, drop_last=True)
+
+running_loss = []
+loss = torch.nn.MSELoss()
+model = BCModelPcl().to(device)
+optim = torch.optim.Adam(model.parameters(), lr=0.001)
+
+for epoch in range(10):
+    for index, (stacked_images, pcl ,local_goal, prev_cmd_vel, gt_cmd_vel) in enumerate(train_loader):
+        
+        stacked_images = stacked_images.to(device)
+        pcl = pcl.to(device)
+        local_goal= local_goal.to(device)
+        prev_cmd_vel= prev_cmd_vel.to(device)
+        gt_cmd_vel= gt_cmd_vel.to(device)
+        # print(f"{gt_cmd_vel.shape = }")
+
+        pred_cmd_vel = model(pcl, stacked_images, local_goal, prev_cmd_vel)
+        # print(f"{pred_cmd_vel.shape = }")
+        error  = loss(pred_cmd_vel, gt_cmd_vel)
+        optim.zero_grad()
+        error.backward()
+        optim.step()
+
+        print(f'step is: {index} and error is: {error.item()}')
+
+        running_loss.append(error.item())
+
+    print(f'epoch is: {epoch} and error is: {sum(running_loss)/len(running_loss)}')
+
+# plt.plot(range(10),running_loss[:10])
+
