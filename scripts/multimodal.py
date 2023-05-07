adb3227fefb78c700bf25e37b8eb0e1e5caf3bd4
bhaba-ranjan <bhabaranjan.cs@gmail.com>
2023-05-07 14:19:48 -0400

    fix: error tracking fixes

--- scripts/multimodal.py
+++ scripts/multimodal.py
@@ -1,15 +1,20 @@
 import torch
 import os
+
 from data_builder.indexer import IndexDataset
 from data_builder.transformer import ApplyTransformation
 from model_builder.multimodal.net import BCModelPcl
 from torch.utils.data import DataLoader
+from tqdm import tqdm
 import matplotlib.pyplot as plt
 import coloredlogs, logging
+
 
 coloredlogs.install()
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
+
+print(f'Using device ========================================>  {device}')
 
 
 def get_data_loader(input_file_path, read_type, batch_size):
@@ -23,7 +28,7 @@ def get_data_loader(input_file_path, read_type, batch_size):
 
 # root = '/Users/bhabaranjanpanigrahi/Research/Code/fusion-network/recorded-data/136021.bag'
 
-root_val = '/Users/bhabaranjanpanigrahi/Research/Code/fusion-network/recorded-data/136021.bag'
+root_val = '/home/ranjan/Workspace/fusion-network/recorded-data/val/138139'
 
 val_indexer = IndexDataset(root_val)
 val_dataset = ApplyTransformation(val_indexer)
@@ -35,7 +40,7 @@ def run_validation(val_loader, model):
        val_error = []
        loss = torch.nn.MSELoss()
        with torch.no_grad():
-        for index, (stacked_images, pcl ,local_goal, prev_cmd_vel, gt_cmd_vel) in enumerate(val_loader):
+        for index, (stacked_images, pcl ,local_goal, prev_cmd_vel, gt_cmd_vel) in tqdm(enumerate(val_loader)):
             stacked_images = stacked_images.to(device)
             pcl = pcl.to(device)
             local_goal= local_goal.to(device)
@@ -48,19 +53,20 @@ def run_validation(val_loader, model):
 
         running_loss = sum(val_error)/len(val_error)
 
-        print(f' ================== Average Validation error is:   {running_loss}  ================== \n')
+        print(f' =======> Average Validation error is:   {running_loss} \n')
         return running_loss
             
 
 
-def run_training(train_files, batch_size):
-    running_loss = []
+def run_training(train_files, batch_size, num_epochs):
     loss = torch.nn.MSELoss()
     model = BCModelPcl().to(device)
-    optim = torch.optim.Adam(model.parameters(), lr=0.001)
-    epoc_error = []
-    running_val_loss = []
-    for epoch in range(10):        
+    error_at_epoch = []
+    val_error_at_epoch = []
+    optim = torch.optim.Adam(model.parameters(), lr=0.0001) 
+    
+    for epoch in range(num_epochs):   
+        running_loss = []
         for train_file in train_files:        
             train_loader = get_data_loader( train_file, 'train', batch_size = batch_size )            
             for index, (stacked_images, pcl ,local_goal, prev_cmd_vel, gt_cmd_vel) in enumerate(train_loader):
@@ -82,14 +88,15 @@ def run_training(train_files, batch_size):
                 print(f'step is:   {index} and error is:   {error.item()} \n')
                 running_loss.append(error.item())
 
-            error_at_epoch = sum(running_loss)/len(running_loss)
-            epoc_error.append(error_at_epoch)
-            print(f'================== epoch is: {epoch} and error is: {error_at_epoch}==================\n')
-            
-            running_val_loss.append(run_validation(val_loader, model))
+        avg_error_at_epoch = sum(running_loss)/len(running_loss)
+        error_at_epoch.append(avg_error_at_epoch)
+        print(f'================== epoch is: {epoch} and error is: {error_at_epoch}==================\n')
+        val_error_at_epoch.append(run_validation(val_loader, model))
 
-    plt.plot(range(len(epoc_error)),epoc_error,'-',color='g', legend= 'Training')
-    plt.plot(range(len(running_val_loss)),running_val_loss,'--',color='r', legend='Testing')
+    torch.save(model.state_dict(), "saved_model.pth")
+    plt.plot(range(len(error_at_epoch)),error_at_epoch,'-',color='g', label= 'Training')
+    plt.plot(range(len(val_error_at_epoch)),val_error_at_epoch,'--',color='r', label='Testing')
+    plt.legend(loc='upper left')
     plt.show()
 
 
@@ -97,4 +104,9 @@ def run_training(train_files, batch_size):
 def main():
     train_dirs = [ os.path.join('../recorded-data/train', dir) for dir in os.listdir('../recorded-data/train')]
     batch_size = 24
-    run_training(train_dirs, batch_size)
+    epochs = 10
+    run_training(train_dirs, batch_size, epochs)
+
+
+
+main()
