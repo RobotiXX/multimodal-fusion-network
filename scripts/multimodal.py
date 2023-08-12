76cd0464787f9cf041683517e4b67dff58c0459d
Bhabaranjan Panigrahi <bpanigr@hop-amd-1.orc.gmu.edu>
2023-08-12 00:08:26 -0400

    fix: resume training

--- scripts/multimodal.py
+++ scripts/multimodal.py
@@ -61,7 +61,7 @@ def get_data_loader(input_file_path, read_type, batch_size):
     logging.info(f'Reading {read_type} file from path {input_file_path}')
     indexer = IndexDataset(input_file_path)
     transformer = ApplyTransformation(indexer)
-    data_loader = DataLoader(transformer, batch_size = batch_size, drop_last=False, prefetch_factor=2,num_workers=12)
+    data_loader = DataLoader(transformer, batch_size = batch_size, drop_last=False, prefetch_factor=2,num_workers=18)
     return data_loader
    
 
@@ -137,7 +137,7 @@ def run_validation(val_files, model, batch_size, epoch, optim):
             torch.save({
             'model_state_dict': model.state_dict(),
             'optimizer_state_dict': optim.state_dict(),
-            }, f'{model_storage_path}/end_to_end_velocities_{epoch+1}_{avg_loss_on_validation}.pth')
+            }, f'{model_storage_path}/resume_end_to_end_velocities_{epoch+1}_{avg_loss_on_validation}.pth')
 
         print(f'=========================> Average Validation error is:   { avg_loss_on_validation } \n')
         return avg_loss_on_validation            
@@ -152,9 +152,9 @@ def run_training(train_files, val_dirs, batch_size, num_epochs):
     # run_validation(val_dirs, model, batch_size, 0, optim)
     # return
     
-    # ckpt = torch.load('/scratch/bpanigr/model_weights/transformer/angler_only_multi_modal_velocities_120.pth')
-    # model.load_state_dict(ckpt['model_state_dict'])
-    scheduler = MultiStepLR(optim, milestones= [30,70,130], gamma=.75)
+    ckpt = torch.load('/scratch/bpanigr/model_weights/end-to-end/end_to_end_velocities_150_0.5871485762779396.pth')
+    model.load_state_dict(ckpt['model_state_dict'])
+    scheduler = MultiStepLR(optim, milestones= [80,160,300], gamma=.9)
 
     data_dict = {}
     for epoch in range(num_epochs):
@@ -246,7 +246,7 @@ def main():
     # # train_dirs.remove('/scratch/bpanigr/fusion-network/recorded-data/train/136514_sw_wt_sc')
     train_dirs.remove('/scratch/bpanigr/fusion-network/recorded-data/train/135967_at')
 
-    batch_size = 100
+    batch_size = 90
     epochs = 350
     run_training(train_dirs, val_dirs, batch_size, epochs)
 
